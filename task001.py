05241ee43662d57f1cd27c658b915158deb67c17
«Artemka» <sergeev_artem91@mail.ru>
2022-05-03 02:29:41 +0500

    Вывести квадрат числа

---
+++ task001.py
@@ -0,0 +1,8 @@
+# Задача 1. Вывести квадрат числа
+
+print('Введите число:')
+a = int(input())
+print('Квадрат числа', a, 'равен', a*a)
+
+number = int(input('Enter the number:'))
+print(f'{number}^2=={number **2}')
